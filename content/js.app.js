5909355e5953f7d22f0b447ca8ff772e9cab9309
Grebbes <Grebbes@gmail.com>
2026-01-19 15:03:41 +0100

    added REST API request

--- content/js.app.js
+++ content/js.app.js
@@ -3,8 +3,21 @@ const initApp = () => {
   loadvehicles();
 };
 
-const loadvehicles = () => {
+const loadvehicles = async () => {
   console.log("Vehicles loading");
+  const Vehicles = await fetch("http://localhost:3000/vehicles");
+
+  if (Vehicles.ok) {
+    const result = await Vehicles.json();
+    console.log(result);
+
+    result.forEach((Vehicle) => {
+      console.log(Vehicle.manufacturer);
+    });
+
+    return;
+  }
+  console.error("hoppsan det gick lite fel");
 };
 
 document;
